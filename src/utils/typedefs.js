0f5de3242e81472be7607bd5ecb02479ebb28a21
Seir <seirdotexe@gmail.com>
2026-09-15 19:45:53 +0200

    feat: initialize env with typings

---
+++ src/utils/typedefs.js
@@ -0,0 +1,14 @@
+/**
+ * --- ENV
+ * @typedef {object} NodeJS.ProcessEnv
+ * Web server settings
+ * @property {string} WEB_HOST
+ * @property {number} WEB_PORT
+ * Game server settings
+ * @property {string} GAME_HOST
+ * @property {number} GAME_PORT
+ * Internal
+ * @property {'development'|'production'} NODE_ENV
+ * @property {string} PROCESS_TITLE
+ * @property {string} VERSION
+ */
