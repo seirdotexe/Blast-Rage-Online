5af296f33c600a55816e0d6565c783a40900e9f6
Seir <seirdotexe@gmail.com>
2026-09-15 19:46:04 +0200

    feat: logger

---
+++ src/utils/logger.js
@@ -0,0 +1,13 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+
+const logger = winston.createLogger({
+  exitOnError: false,
+  levels: { error: 0, warn: 1, verbose: 2, info: 3 }
+});
+
+logger.add(new winston.transports.Console({
+  format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+}));
+
+export default logger;
--- src/utils/typedefs.js
+++ src/utils/typedefs.js
@@ -11,4 +11,10 @@
  * @property {'development'|'production'} NODE_ENV
  * @property {string} PROCESS_TITLE
  * @property {string} VERSION
+ * --- General purpose
+ * @typedef {Object} ILogger
+ * @property {(msg:string)=>void} error
+ * @property {(msg:string)=>void} warn
+ * @property {(msg:string)=>void} info
+ * @property {(msg:string)=>void} verbose
  */
