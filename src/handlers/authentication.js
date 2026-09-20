fb82bda6d2c93e90909d74daac9970490c5b9192
Seir <seirdotexe@gmail.com>
2026-09-20 22:14:47 +0200

    feat: start working on login packet

--- src/handlers/authentication.js
+++ src/handlers/authentication.js
@@ -22,8 +22,7 @@ export default {
       if (!correctPassword) return client.send('09');
 
       await client.setClient(userObj);
-
-      // Todo - Send packet
+      client.send(`0A${client.username}|${client.current_bits_balance}|${client.total_bits_earned}|${client.xcash}|${client.id}`); // Todo - Send packet
     } catch (err) {
       client.logger.error('Error while handling authentication', err);
     }
