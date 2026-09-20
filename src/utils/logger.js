6c0de25a071b8b64415ec496663d7a33aba97f4c
Seir <seirdotexe@gmail.com>
2026-09-20 21:55:44 +0200

    feat: better logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,6 +3,8 @@ import 'winston-daily-rotate-file';
 
 const logger = winston.createLogger({
   exitOnError: false,
+  handleExceptions: true,
+  handleRejections: true,
   levels: { error: 0, warn: 1, verbose: 2, info: 3 }
 });
 
