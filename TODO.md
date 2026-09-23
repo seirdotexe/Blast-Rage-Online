831e55fe0b61d4c3bf029caca06d8e3d640a5513
Seir <seirdotexe@gmail.com>
2026-09-23 19:57:58 +0200

    feat: work on web - register users

--- TODO.md
+++ TODO.md
@@ -9,15 +9,3 @@
 # API
 
 - We can use Murray's ID `8060894`
-
-GET `http://api.xgenstudios.com/?method=xgen.users.add&username=&password=&email_address=&v=2`
-Content-Type:"text/xml"
-```xml
-<?xml version="1.0" encoding="utf-8" ?><rsp stat="fail">
-	<err code="4" msg="Username already exists" />
-</rsp>
-
-<?xml version="1.0" encoding="utf-8" ?><rsp stat="ok">
-	<user id="0" />
-</rsp>
-```
--- src/web.js
+++ src/web.js
@@ -1,6 +1,7 @@
 import fastifyStatic from '@fastify/static';
 import Fastify from 'fastify';
 import logger from './utils/logger.js';
+import { hash } from './utils/scrypt.js';
 
 export default class WebServer {
   /**
@@ -35,13 +36,40 @@ export default class WebServer {
 
     // Todo - user_id 8060894
     this.#app.get('/csv/', async (req, res) => {
-      if (req?.query?.method === 'xgen.blastrage.user.items.list') {
+      const method = req?.query?.method;
+
+      if (method === 'xgen.blastrage.user.items.list') {
         // Todo - _SafeStr_1077
-      } else if (req?.query?.method === 'xgen.blastrage.user.tanks.list') {
+      } else if (method === 'xgen.blastrage.user.tanks.list') {
         // Todo - _SafeStr_1113
       }
 
       return res.type('text/plain').send('');
+    });
+
+    this.#app.get('/', async (req, res) => {
+      const method = req?.query?.method;
+
+      if (!method) return res.sendFile('index.html');
+
+      if (method === 'xgen.users.add') {
+        const { username, password } = req.query;
+        const userObj = await this.database('users').where({ username }).first();
+
+        if (userObj) {
+          return res.type('text/xml').send(`<?xml version="1.0" encoding="utf-8" ?><rsp stat="fail"><err code="4" msg="Username already exists" /></rsp>`);
+        }
+
+        try {
+          const [id] = await this.database('users').insert({ username, password: await hash(password) });
+
+          this.logger.info(`User ${username} with ${id} has been registered`);
+          return res.type('text/xml').send(`<?xml version="1.0" encoding="utf-8" ?><rsp stat="ok"><user id="${id}" /></rsp>`);
+        } catch (err) {
+          this.logger.error('Error while inserting new user', err);
+          return res.type('text/xml').send(`<?xml version="1.0" encoding="utf-8" ?><rsp stat="fail"><err code="4" msg="Database error" /></rsp>`);
+        }
+      }
     });
 
     this.#app.setNotFoundHandler((_, res) => res.code(404).type('text/html').send('Not Found'));
